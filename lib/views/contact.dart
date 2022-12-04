e8aaf437968be8146fc037c2d5a263ee3a45ab68
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2022-12-03 20:02:03 -0500

    bloc'ing contact image

--- lib/views/contact.dart
+++ lib/views/contact.dart
@@ -1,9 +1,12 @@
+import 'package:dzr_site/bloc/firebase_bloc.dart';
 import 'package:dzr_site/components/components.dart';
 import 'package:dzr_site/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ContactView extends StatelessWidget {
   const ContactView({super.key});
+  final path = "misc/contact";
 
   @override
   Widget build(BuildContext context) {
@@ -11,27 +14,44 @@ class ContactView extends StatelessWidget {
         child: Column(
       children: [
         SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
+          height: screenSize(context).height,
+          width:screenSize(context).width,
           child: Stack(
             alignment: Alignment.bottomCenter,
             children: [
-              Positioned(
-                top: MediaQuery.of(context).size.height / -1,
-                child: const Image(
-                  image: AssetImage("dan.jpg"),
+              BlocProvider(
+                create: (context) =>
+                    FirebaseBloc()..add(FirebaseFetch(path: path)),
+                child: BlocBuilder<FirebaseBloc, FirebaseState>(
+                  builder: (context, state) {
+                    if (state is FirebaseLoaded) {
+                      return Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          Positioned(
+                              top: screenSize(context).height / -1,
+                              child: state.firebaseImages[0].img!),
+                          Positioned(
+                            top: screenSize(context).height / 5,
+                            child: Container(
+                              decoration: titleBox,
+                              child: Text("CONTACT US",
+                                  style: titleStyle,
+                                  textAlign: TextAlign.center),
+                            ),
+                          ),
+                        ],
+                      );
+                    } else if (state is FirebaseError) {
+                      return Text(state.error.toString());
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
                 ),
               ),
               Positioned(
-                top: MediaQuery.of(context).size.height / 5,
-                child: Container(
-                  decoration: titleBox,
-                  child: Text("CONTACT US",
-                      style: titleStyle, textAlign: TextAlign.center),
-                ),
-              ),
-              Positioned(
-                top: MediaQuery.of(context).size.height / 3,
+                top: screenSize(context).height / 3,
                 child: const ContactForm(),
               ),
             ],
