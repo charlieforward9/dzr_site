e6c84c6cb3d5edf6e267183698a92fc1bb6d3cc4
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2022-12-03 19:47:26 -0500

    remaking the process widget

---
+++ lib/components/claims_process.dart
@@ -0,0 +1,51 @@
+import 'package:dzr_site/components/components.dart';
+import 'package:dzr_site/styles.dart';
+import 'package:flutter/material.dart';
+
+class ClaimsProcess extends StatelessWidget {
+  const ClaimsProcess({super.key});
+
+  static final List<String> process = [
+    "LIFE & SAFETY ISSUES",
+    "MITIGATION OF DAMAGES",
+    "ESTABLISH A RECOVERY STRATEGY",
+    "ANALYSIS OF COVERAGES",
+    "VALUATION OF DAMAGES",
+    "NEGOTIATIONS & SETTLEMENT",
+    "RESTORATION OF PROPERTY & OPERATIONS",
+    "ANOTHER HAPPY CLAIMPROS CLIENT!",
+  ];
+
+  @override
+  Widget build(BuildContext context) => ConstrainedBox(
+        constraints: const BoxConstraints(
+            minHeight: 400, maxHeight: 650, minWidth: 600.0),
+        child: Column(
+          children: [
+            Text("CLAIMS RECOVERY PROCESS",
+                style: titleStyle, textAlign: TextAlign.center),
+            Column(
+                children: process
+                    .map<Widget>((i) => Builder(
+                        builder: (BuildContext context) => Container(
+                            alignment: Alignment.center,
+                            color: process.indexOf(i) == process.length - 1
+                                ? actionColor
+                                : secondaryColor
+                                    .withOpacity(process.indexOf(i) / 10 + 0.1),
+                            height: MediaQuery.of(context).size.height / 18,
+                            child: Text(
+                              i,
+                              softWrap: true,
+                              textAlign: TextAlign.center,
+                              style: captionStyle,
+                              maxLines: 1,
+                            ))))
+                    .toList()),
+            const Spacer(),
+            const ContactButton(),
+            const Spacer(flex: 2),
+          ],
+        ),
+      );
+}
